4520845bf44d431662897f6de05a74238b69f9ee
Emin Martinian <emin.martinian@gmail.com>
2017-03-17 15:57:16 -0400

    Simple settings now include stub database

--- ox_herd/settings.py
+++ ox_herd/settings.py
@@ -1,3 +1,9 @@
 """Module to represent basic settings for ox_herd package.
 """
 
+# Stub user database used only for testing stand alone ox_herd.
+
+STUB_USER_DB = {
+    'test' : 'test_pw'
+    }
+
