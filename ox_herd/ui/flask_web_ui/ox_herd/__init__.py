aa0c9e4bc1bb206b28e3197a51fddce7f01b493a
Emin Martinian <emin.martinian@gmail.com>
2017-03-17 13:18:29 -0400

    Added url_prefix='/ox_herd'

--- ox_herd/ui/flask_web_ui/ox_herd/__init__.py
+++ ox_herd/ui/flask_web_ui/ox_herd/__init__.py
@@ -4,5 +4,5 @@
 from flask import Blueprint
 
 OX_HERD_BP = Blueprint('ox_herd', __name__, template_folder='templates',
-                       static_folder='static')
+                       static_folder='static', url_prefix='/ox_herd')
 
