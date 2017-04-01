698e7197fd179afefc6b41b3110ad5879508d8cd
Emin Martinian <emin.martinian@gmail.com>
2017-04-01 15:51:50 -0400

    Added REDIS_PREFIX and made RUN_DB default to redis.

--- ox_herd/settings.py
+++ ox_herd/settings.py
@@ -13,6 +13,10 @@ STUB_USER_DB = {
 QUEUE_NAMES = 'default'
 
 # Optional pair representing mode and string path to where we store
-# database tracking job execution. Default mode is 'sqlite' with
+# database tracking job execution. Default mode is ('redis', None)
+# to just use redis. You can also use 'sqlite' with
 # either a path to the sqliet db or None to use a default path.
-RUN_DB = ('sqlite', None)
+RUN_DB = ('redis', None)
+
+# Prefix to use in things we store on redis queue for ox_herd.
+REDIS_PREFIX = 'ox_herd:'
