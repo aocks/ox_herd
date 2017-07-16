7419acc379af8d2a60f3089e9582b246974274e3
Emin Martinian <emin.martinian@gmail.com>
2017-07-16 15:47:58 -0400

    Put in OX_HERD_CONF and added docs

--- ox_herd/settings.py
+++ ox_herd/settings.py
@@ -1,11 +1,30 @@
 """Module to represent basic settings for ox_herd package.
 """
 
-# Stub user database used only for testing stand alone ox_herd.
+import os
 
-STUB_USER_DB = {
-    'test' : 'test_pw'
-    }
+# Path to your ox herd configuration file.
+OX_HERD_CONF = os.environ.get('OX_HERD_CONF', os.path.join(
+    os.environ.get('HOME', ''), '.ox_herd_conf'))
+
+# Stub user database used only for testing stand alone ox_herd.
+# You can either manually configure this to have keys as usernames
+# and password hashes created according to the following:
+#
+# >>> from passlib.apps import custom_app_context as pwd_context
+# >>> hash = pwd_context.hash("somepass")
+#
+# or provide a [STUB_USER_DB] section in OX_HERD_CONF with username
+# as keys and password hashes as values
+#
+#                       WARNING!
+#
+# If you really want security, you will also need to setup SSL.
+# You should probably setup SSL and some better password security
+# in your app and just use ox_herd as a Flask blueprint to inherit
+# proper security instead of using the login stub but it is there for
+# ease of testing and local use.
+STUB_USER_DB = {}
 
 # Space separated list of queue names to show in showing scheduled tests.
 # Can be changed by your own software after importing settings if you like.
