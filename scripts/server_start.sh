a78a8c311df65fbd1f415747db967b2f34b65753
Emin Martinian <emin.martinian@gmail.com>
2017-07-16 15:51:55 -0400

    Show some git log output when starting

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -8,6 +8,7 @@ su - -c rqscheduler ox_user 2>&1 | \
    rotatelogs /home/ox_user/ox_server/logs/rqscheduler.log 86400 &
 
 export PYTHONPATH=/home/ox_user/ox_server/ox_herd
+echo "output of git log is: `git log | head -7`"
 echo "PYTHONPATH is $PYTHONPATH"
 python3 /home/ox_user/ox_server/ox_herd/ox_herd/scripts/serve_ox_herd.py
 
