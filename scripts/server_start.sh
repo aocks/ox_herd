147811f16e75d43705a5f52dbd7e61a059d0e0d5
Emin Martinian <emin.martinian@gmail.com>
2017-07-16 20:55:56 -0400

    Restart server and rotate logs for main python application

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -2,12 +2,16 @@
 
 # This is a script used to start everything up on docker.
 
+# Restart redis server just to make sure it is up and working.
+/etc/init.d/redis-server restart
+
 su - -c "rq worker" ox_user 2>&1 | \
-   rotatelogs /home/ox_user/ox_server/logs/rq_worker.log 86400 &
+   rotatelogs /home/ox_user/ox_server/logs/rq_worker.log 86400 10M &
 su - -c rqscheduler ox_user 2>&1 | \
-   rotatelogs /home/ox_user/ox_server/logs/rqscheduler.log 86400 &
+   rotatelogs /home/ox_user/ox_server/logs/rqscheduler.log 86400 10M&
 
 export PYTHONPATH=/home/ox_user/ox_server/ox_herd
 echo "git log is: `cd /home/ox_user/ox_server/ox_herd && git log | head -7`"
 echo "PYTHONPATH is $PYTHONPATH"
-su - -c "python3 /home/ox_user/ox_server/ox_herd/ox_herd/scripts/serve_ox_herd.py" ox_user 2>&1 | tee /home/ox_user/ox_server/logs/ox_herd.log
+su - -c "python3 /home/ox_user/ox_server/ox_herd/ox_herd/scripts/serve_ox_herd.py" ox_user 2>&1 | \
+   rotatelogs -e -f /home/ox_user/ox_server/logs/serve_ox_herd.log 86400 10M
