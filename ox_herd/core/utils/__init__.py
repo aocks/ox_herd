93b24f34c8b8a77e5d898a46deb32f0acbb91111
Emin Martinian <emin.martinian@gmail.com>
2018-10-19 11:20:14 -0400

    Added utils with ox_decs.py

---
+++ ox_herd/core/utils/__init__.py
@@ -0,0 +1,2 @@
+"""The ox_herd.core.utils package contains useful utilities for ox_herd.
+"""
---
+++ ox_herd/core/utils/ox_decs.py
@@ -0,0 +1,158 @@
+"""Useful decorators and decorator tools.
+
+The `ox_decs` module contains useful decorators and related tools.
+One of the most useful is the `withdoc` decorator which can make
+sure decorated functions have their original documentation plus
+some documentation about how they were decorated.
+
+See docs for `withdoc` for more information.
+"""
+
+import doctest
+import time
+import logging
+import functools
+
+
+def withdoc(decorator):
+    """Decorator to apply a decorator and attach its docs to a function.
+
+    The following illustrates example usage:
+
+>>> import functools
+>>> from ox_decs import withdoc
+>>> def withprint(func):
+...     "withprint decorates a function to print result after running it."
+...     @functools.wraps(func)
+...     def decorated(*args, **kwargs):
+...         result = func(*args, **kwargs)
+...         print('Output of %s is %s' % (func.__name__, result))
+...         return result
+...     return decorated
+...
+>>> @withprint
+... def add(x, y):
+...    "Add inputs x and y together"
+...    return x + y
+...
+>>> print(add.__doc__)  # Note that it does *NOT* mention decoration
+Add inputs x and y together
+>>> @withdoc(withprint)
+... def combine(x, y):
+...    "Add inputs x and y together"
+...    return x + y
+...
+>>> print(combine.__doc__)  # Note that it *DOES* mention decoration
+Add inputs x and y together
+-------
+Wrapped by decorator withprint:
+withprint decorates a function to print result after running it.
+
+    """
+    @functools.wraps(decorator)
+    def dec_with_doc(func):
+        "Create decorator with doc."
+        fix_doc(func, decorator)
+        return decorator(func)
+    return dec_with_doc
+
+
+def fix_doc(func, decorator):
+    """Fix a decorated function with docs about its decorator
+
+    :param func:        Decorated function.
+
+    :param decorator:   The decorator that decorated func.
+
+    ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
+
+    PURPOSE:   Modify func.__doc__ to describe the decorator.
+               Intended to be used by `withdoc` function. See that
+               for more details.
+    """
+    if not func.__doc__:
+        func.__doc__ = 'Function %s' % func.__name__
+    extra = '\n-------\nWrapped by decorator %s' % getattr(
+        decorator, '__name__', '(unknown)')
+    if decorator.__doc__:
+        extra += ':\n%s' % decorator.__doc__
+    func.__wrapdoc__ = extra
+    func.__doc__ += extra
+
+
+def withlog(func, logfunc=logging.info):
+    "Decorate function to log output after calling."
+
+    @functools.wraps(func)
+    def decorated(*args, **kwargs):
+        "Function with logging"
+        result = func(*args, **kwargs)
+        name = getattr(func, '__name__', '(unknown)')
+        logfunc('Calling %s gives:\n%s\n', name, result)
+        return result
+    fix_doc(decorated, withlog)
+
+    return decorated
+
+
+def withtime(func, logfunc=logging.info):
+    "Decorate function to show run time after calling."
+
+    @functools.wraps(func)
+    def decorated(*args, **kwargs):
+        "Function with time"
+        start = time.time()
+        result = func(*args, **kwargs)
+        secs = time.time() - start
+        name = getattr(func, '__name__', '(unknown)')
+        logfunc('Calling %s took %.3f seconds' % (name, secs))
+        return result
+    return decorated
+
+
+def composed(*decs):
+    """Decorator to compose other decorators together.
+
+>>> import functools
+>>> from ox_decs import withdoc, composed, withlog, withtime
+>>> def withprint(func):
+...     "withprint decorates a function to print result after running it."
+...     @functools.wraps(func)
+...     def decorated(*args, **kwargs):
+...         result = func(*args, **kwargs)
+...         print('Output of %s is %s' % (func.__name__, result))
+...         return result
+...     return decorated
+...
+>>> @composed(withprint, withlog, withtime)
+... def combine(x, y):
+...    "Add inputs x and y together"
+...    return x + y
+...
+>>> print(combine.__doc__)  # Note that it *DOES* mention decoration
+Add inputs x and y together
+-------
+Wrapped by decorator withtime:
+Decorate function to show run time after calling.
+-------
+Wrapped by decorator withlog:
+Decorate function to log output after calling.
+-------
+Wrapped by decorator withprint:
+withprint decorates a function to print result after running it.
+
+    """
+    def deco(func):
+        "Composed decorator (decorator 0 applied first)"
+        for dec in reversed(decs):
+            orig = func
+            func = dec(func)
+            if orig.__doc__ == func.__doc__:
+                fix_doc(func, dec)
+        return func
+    return deco
+
+
+if __name__ == '__main__':
+    doctest.testmod()
+    print('Finished tests')
