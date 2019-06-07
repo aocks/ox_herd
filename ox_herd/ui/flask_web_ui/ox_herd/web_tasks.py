49e2a099e1067af8c398199f72f3c42262fe1805
emin-ec <emin@empower-capital.com>
2019-06-07 14:22:17 -0400

    More improvements for web task such as setting base url

--- ox_herd/ui/flask_web_ui/ox_herd/web_tasks.py
+++ ox_herd/ui/flask_web_ui/ox_herd/web_tasks.py
@@ -3,6 +3,7 @@
 
 import re
 import logging
+import urllib.parse
 
 import requests
 
@@ -19,7 +20,7 @@ and inspect task results with other tools.
 
     def __init__(self, return_value: str, full_text: str = None,
                  status_code: int = 0, reason: str = 'OK',
-                 json_result: str = ''):
+                 extras: dict = None):
         """Initializer.
 
         :param return_value:str: String return value to display as task
@@ -36,17 +37,14 @@ and inspect task results with other tools.
         :param reason:str='OK': String reason provided for HTTP response. Use
                                 'NA' if not an HTTP response.
 
-        :param json_result:str='':  String in JSON format describing result.
-                                    This is useful if you want to return
-                                    arbirary data. Use '' if no JSON data
-                                    provided.
+        :param extras=None:  Optional dict of additional data.
 
         """
         self.return_value = return_value
         self.full_text = full_text if full_text is not None else return_value
         self.status_code = status_code
         self.reason = reason
-        self.json_result = json_result
+        self.extras = extras if extras else {}
 
     @classmethod
     def fields(cls) -> list:
@@ -58,18 +56,18 @@ in to_dict.
 
         _ = cls
         return ['return_value', 'full_text', 'status_code',
-                'reason', 'json_result']
+                'reason', 'extras']
 
     def to_dict(self) -> dict:
         "Return dict with data in self.fields()"
-        return {n: getattr(n) for n in self.fields()}
+        return {n: getattr(self, n) for n in self.fields()}
 
 
 class SimpleWebTask(ox_tasks.OxHerdTask, base.OxPluginComponent):
     """Generic command that can be sub-classed for automation.
     """
 
-    def __init__(self, *args, base_url=None, **kwargs):
+    def __init__(self, *args, base_url=None, base_port='', **kwargs):
         """Initializer.
 
         :arg base_url=None:        String url for where your server lives (or
@@ -78,22 +76,80 @@ class SimpleWebTask(ox_tasks.OxHerdTask, base.OxPluginComponent):
         if not args and 'name' not in kwargs:
             kwargs['name'] = self.__class__.__name__
         ox_tasks.OxHerdTask.__init__(self, *args, **kwargs)
-        self.base_url = base_url if base_url else self.make_base_url()
+
+        # Never use base_url/base_port directly. Instead call
+        # set_base_url and make_url so we handle ports correctly.
+        self.__base_url = ''
+        self.__base_port = None
+        self.set_base_url(base_url, base_port)
+
+    def set_base_url(self, base_url: str, base_port: str = None):
+        """Set the base_url (and optinally base_port).
+
+        :param base_url:    String like http://foo or http://bar:9999
+
+        :param base_port=None: Optional base_port if not in base_url.
+
+        ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
+
+        PURPOSE:  Set base URL and port to use in connections.
+
+        """
+        self.__base_url = base_url if base_url else self.make_base_url()
+        if not self.__base_url:
+            return
+        my_base, my_port = urllib.parse.splitport(self.__base_url)
+        if my_port:
+            self.__base_url = my_base  # take off port and store in base_port
+            if base_port:
+                raise ValueError('base_url=%s has port and base_port=%s' % (
+                    self.__base_url, base_port))
+            base_port = my_port
+        self.__base_port = base_port
+
+    def make_url(self, path: str = '') -> str:
+        """Make url with given path.
+
+        :param path='': Optional relative path.
+
+        ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
+
+        :return:  Full string URL.
+
+        ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
+
+        PURPOSE:  Combine base URL and port set with set_base_url with path.
+
+        """
+        result = self.__base_url
+        if not result:
+            logging.error('self.__dict__: %s', str(self.__dict__))
+            raise ValueError('Must have base_url set to call make_url')
+        if self.__base_port:
+            result += ':' + str(self.__base_port)
+        result += '/' + path.lstrip('/')
+
+        return result
 
     @classmethod
     def make_base_url(cls) -> str:
-        """Return string indicating base URL.
+        """Return string indicating base URL or None if not known.
 
-Sub-classes must overide.
+Sub-classes can override if they know a good default. Otherwise,
+should call set_base_url when they have the url to use.
         """
-        raise NotImplementedError
+        return None
 
-    def get_username(self) -> str:
-        """Return string username for login.
+    def get_login_info(self) -> (str, str):
+        """Return string login field and login value
 
 Sub-classes may want to override.
         """
-        return getattr(self, 'username', 'test_user')
+        return 'username', getattr(self, 'username', 'test_user')
+
+    def get_password(self, login_name: str) -> str:
+        "Get password for given login name"
+        return self.get_secret(login_name, 'test_passwords')
 
     @classmethod
     def get_login_route(cls) -> str:
@@ -124,16 +180,30 @@ Sub-classes may want to override.
     def setup_session(self):
         """Setup a requests.session and return it.
 
-This sets up a session so we are logged in to whatever self.base_url
-is pointing at via the route at get_login_route() using the user
-in get_username().
+This sets up a session so we are logged in to whatever we get
+from self.make_url(self.get_login_route()) using the login from
+get_login_info().
         """
         session = requests.session()
-        username = self.get_username()
-        password = self.get_secret(username, 'testing_password')
-        session.post('%s%s' % self.base_url, self.get_login_route(),
-                     {'username': username, 'password': password},
-                     verify=False)
+        my_url = self.make_url(self.get_login_route())
+        csrf_field, csrf = None, None
+        try:
+            csrf_field, csrf = self.get_csrf_from_form(session, my_url)
+        except Exception as problem:
+            logging.error('Failed to get csrf due to exception: %s', str(
+                problem))
+            logging.error('Maybe override get_csrf_from_form?')
+            raise
+
+        login_field, login_name = self.get_login_info()
+        password = self.get_password(login_name)
+        data = {login_field: login_name, 'password': password}
+        if csrf:
+            data[csrf_field] = csrf
+        post_resp = session.post(my_url, data, verify=False)
+        if post_resp.status_code != 200:
+            raise ValueError('Got unexpected status/reason: %s/%s in login' % (
+                post_resp.status_code, post_resp.reason))
         return session
 
     def do_main(self, session) -> SimpleTaskResult:
@@ -175,7 +245,9 @@ for more details.
                      comment)
 
     @staticmethod
-    def get_csrf_from_form(session, url):
+    def get_csrf_from_form(
+            session, url: str, csrf_field: str = 'csrf_token',
+            csrf_re: str = None) -> (str, str):
         """Do a get request for the given url and extract CSRF token.
 
         :param session:    Session we have to the web site.
@@ -184,7 +256,7 @@ for more details.
 
         ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
 
-        :return:  String for CSRF token.
+        :return:  String name for CSRF field and string for CSRF token.
 
         ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
 
@@ -194,13 +266,15 @@ for more details.
                   our POST request. This function gets the csrf_token.
         """
         result = session.get(url)
-        csrf_re = re.compile(' *'.join([
-            'id="csrf_token"', 'name="csrf_token"', 'type="hidden"',
-            'value="(?P<csrf>[^"]*)">']))
+        csrf_re = csrf_re if csrf_re else ' *'.join([
+            'id="%s"' % csrf_field, 'name="%s"' % csrf_field,
+            'type="hidden"', 'value="(?P<csrf>[^"]*)">'])
+        if isinstance(csrf_re, str):
+            csrf_re = re.compile(csrf_re)
         match = csrf_re.search(result.text)
         if not match:
             raise ValueError('Could not extract csrf from url "%s"' % url)
-        return match.group('csrf')
+        return csrf_field, match.group('csrf')
 
     def raise_on_bad_status(self, result):
         """Raise ValueError if http response looks like an error.
